94d351fe0681d08238fd6cec0e1a27a4933b5cdf
Ewnetu Bayuh Lakew <ewnetu@cs.umu.se>
2017-04-25 00:28:03 +0200

    Update vm-operations.py

--- vm-operations.py
+++ vm-operations.py
@@ -2,61 +2,119 @@ from keystoneauth1.identity import v2
 from keystoneauth1 import session
 from novaclient.client import Client as NovaClient
 import datetime
-username=‘put your username’
-password=‘put your password’
-tenant_name='CloudCourse’#make sure you are using the right project name
-auth_url='http://94.246.116.242:5000/v2.0'
-auth = v2.Password(username=username, password=password, tenant_name=tenant_name, auth_url=auth_url)
-sess = session.Session(auth=auth)
-nova = NovaClient("2", session = sess)
-
-def monitoringInfo(tenant_name,start_date,end_date):
-  usage = nova.usage.get(tenant_name,start_date,end_date)
-  print usage
-
-
-def createFloatingIP(VMName):
-  nova.floating_ip_pools.list()
-  floating_ip = nova.floating_ips.create(nova.floating_ip_pools.list()[0].name)
-  print("floating IP %s is assigned to %s VM", floating_ip.ip, name)
-  instance = nova.servers.find(name=VMName)
-  instance.add_floating_ip(floating_ip)
-
-def createVM(VMName):
- # nova.servers.list()
-        image = nova.images.find(name="Ubuntu 16.04 LTS")#nova.images.find(name="Test") #
-        flavor = nova.flavors.find(name="m1.medium")
-        net = nova.networks.find(label="CloudCourse")
+import argparse
+import ConfigParser
+class OpenStackVMOperations:
+    def readConf(self):
+       
+        config = ConfigParser.RawConfigParser()
+        config.read('config.properties')
+        self.openStackUsername=config.get('user', 'username')
+        self.openStackPassword=config.get('user', 'password')
+        self.tenantName=config.get('cloud', 'projectName')
+        self.openStackAuthUrl=config.get('authUrl', 'password')
+       
+    def __init__(self):
+        self.readConf()
+        self.auth = v2.Password(username=self.openStackUsername, password=self.openStackPassword,
+                                 tenant_name=self.tenantNname, auth_url= self.openStackAuthUrl)
+        self.sess = session.Session(auth=auth)
+        self.nova = NovaClient("2", session=sess)
+        
+    def monitoringInfo( start_date, end_date):
+        usage = self.nova.usage.get( self.tenantNname, start_date, end_date)
+        print usage
+    
+    
+    def createFloatingIP(self, VMName):
+        self.nova.floating_ip_pools.list()
+        floating_ip = self.nova.floating_ips.create(self.nova.floating_ip_pools.list()[0].name)
+        print("floating IP %s is assigned to %s VM", floating_ip.ip, name)
+        instance = self.nova.servers.find(name=VMName)
+        instance.add_floating_ip(floating_ip)
+        
+    def createVM(self, VMName):
+     # nova.servers.list()
+        image = self.nova.images.find(name="Ubuntu 16.04 LTS")  # nova.images.find(name="Test") #
+        flavor = self.nova.flavors.find(name="m1.medium")
+        net = self.nova.networks.find(label="CloudCourse")
         nics = [{'net-id': net.id}]
-        vm = nova.servers.create(name=VMName, image=image, flavor=flavor, key_name="ewnetu", nics=nics, userdata=open("vm-init.sh"))
-
-def terminateVM(VMName):
-  instance = nova.servers.find(name=VMName)
-  if instance==None :
-    print("server %s does not exist" % VMName)
-  else:
-    print("deleting server..........")
-    nova.servers.delete(instance)
-    print("server %s deleted" % VMName)
-
-def listFloatingIPs(VMName):
-   ip_list=nova.floating_ips.list()
-   for ip in ip_list:
-     print("fixed_ip : %s\n" % ip.fixed_ip)
-     print("ip : %s" % ip.ip)
-     print("instance_id : %s" % ip.instance_id)
-
-def getVMIP(VMName):
-  instance = nova.servers.find(name=VMName)
-  ip=instance.networks['private']
-  print ("ipaddress:"+ip);
-def getVMDetail(VMName):
-    instance = nova.servers.find(name=VMName)
-    print("server id: %s\n" % instance.id)
-    print("server name: %s\n" % instance.name)
-    print("server image: %s\n" % instance.image)
-    print("server flavor: %s\n" % instance.flavor)
-    print("server key name: %s\n" % instance.key_name)
-    print("user_id: %s\n" % instance.user_id)
+        vm = self.nova.servers.create(name=VMName, image=image, flavor=flavor, key_name="ewnetu", nics=nics, userdata=open("vm-init.sh"))
+    
+    def terminateVM(self,VMName):
+        instance = nova.servers.find(name=VMName)
+        if instance == None :
+            print("server %s does not exist" % VMName)
+        else:
+            print("deleting server..........")
+            nova.servers.delete(instance)
+            print("server %s deleted" % VMName)
+        
+    def listFloatingIPs():
+        ip_list = nova.floating_ips.list()
+        for ip in ip_list:
+             print("fixed_ip : %s\n" % ip.fixed_ip)
+             print("ip : %s" % ip.ip)
+             print("instance_id : %s" % ip.instance_id)
+    
+    def listVMs():
+        vm_list = nova.servers.list()
+        for vm in vm_list:
+            print("########################## #################\n")
+            print("server id: %s\n" % instance.id)
+            print("server name: %s\n" % instance.name)
+            print("server image: %s\n" % instance.image)
+            print("server flavor: %s\n" % instance.flavor)
+            print("server key name: %s\n" % instance.key_name)
+            print("user_id: %s\n" % instance.user_id)
+            print("user_id: %s\n" % instance.networks)
+            print("########################## #################\n\n")
+          
+    
+    def getVMIP(self,VMName):
+        instance = nova.servers.find(name=VMName)
+        ip = instance.networks
+        print ("ipaddress:" + ip)
+        
+        
+    def getVMDetail(self,VMName):
+        instance = nova.servers.find(name=VMName)
+        print("server id: %s\n" % instance.id)
+        print("server name: %s\n" % instance.name)
+        print("server image: %s\n" % instance.image)
+        print("server flavor: %s\n" % instance.flavor)
+        print("server key name: %s\n" % instance.key_name)
+        print("user_id: %s\n" % instance.user_id)
+        print("user_id: %s\n" % instance.networks)
+     
+    def  getOperation(self, args): 
+        if args.operation == "listIP":
+            self.listFloatingIPs()
+        elif args.operation == "listVM":
+            self.listVMs()
+        elif args.operation == "create":
+            self.createVM(args.name)
+        elif args.operation == "terminate":
+            self.terminateVM(args.name)
+        elif args.operation == "assignFIP":
+            self.createFloatingIP(args.name)
+        elif args.operation == "monitor":
+            self.createVM(args.name)
+   
 if __name__ == '__main__':
-      createVM(“WASP”)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--operation",
+        metavar = "VM_OPERATION",
+        help = "The operation that you want to perform",
+        required = True,
+        choices=["create","listVM","terminate","listIP","assignFIP","monitor"],
+        dest="operation")
+
+    parser.add_argument("-n", "--name",
+        metavar = "VM_NAME",
+        help = "The name  for the VM that you want to perform the operation",
+        dest="name")
+    args = parser.parse_args()
+    ops =OpenStackVMOperations()
+    ops.getOperation(args)
+    
