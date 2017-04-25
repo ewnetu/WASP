247030fb4fae59cff14edca0499b58442abc7813
Ewnetu Bayuh Lakew <ewnetu@cs.umu.se>
2017-04-25 08:58:19 +0200

    Update receiverRabbit.py

--- receiverRabbit.py
+++ receiverRabbit.py
@@ -22,7 +22,7 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 channel.basic_consume(callback,
-                      queue='hello',
+                      queue='wasp',
                       no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
