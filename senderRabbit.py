3a01b005c83c0b008d1dc8edf04ea31544de6956
Ewnetu Bayuh Lakew <ewnetu@cs.umu.se>
2017-04-25 08:57:57 +0200

    Update senderRabbit.py

--- senderRabbit.py
+++ senderRabbit.py
@@ -15,10 +15,10 @@ credentials = pika.PlainCredentials(rabbitUser, rabbitPassword)
 connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitServer,rabbitPort,'/',credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='wasp')
 
 channel.basic_publish(exchange='',
-                      routing_key='hello',
+                      routing_key='wasp',
                       body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
