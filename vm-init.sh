d898bebc747c22f6d22017a6c91417cbd9fb7443
Ewnetu Bayuh Lakew <ewnetu@cs.umu.se>
2017-04-26 21:58:22 +0200

    Update vm-init.sh

--- vm-init.sh
+++ vm-init.sh
@@ -5,6 +5,19 @@ sudo apt-get -y update
 sudo apt-get install -y python-dev
 sudo apt-get install -y python-pip
 
+# install python Flask web framework
+sudo pip install Flask
 
+# prepare install directory for application 
+mkdir /var/www
+cd /var/www
+
+#echo "Cloning repo of the WASPY microservice"
+git clone https://github.com/muyiibidun/WASP.git
+
+
+#run you code HERE!
+cd WASP/waspy
+python start.py
 
 
